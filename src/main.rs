963e2a388ec4e0be6c7440db168c121a72a807df
Juuso Takalainen <juuso.takalainen@streamr.com>
2019-10-07 17:42:42 +0900

    real first version with test image

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,17 @@ fn main() {
 
     println!("Drawing {} x {} image", width, height);
 
-    let mut img = ImageBuffer::new(100, 100);
+    let mut img = ImageBuffer::new(width, height);
 
     let size = min(width, height);
 
     for (px, py, pixel) in img.enumerate_pixels_mut() {
         let x = (px as f64 / size as f64) - 0.5;
         let y = (py as f64 / size as f64) - 0.5;
-        let r = x as u8;
-        let g = y as u8;
+        let r = (120.0 + 100.0 * x) as u8;
+        let g = (120.0 + 100.0 * y) as u8;
         let b = 0 as u8;
+        //print!("({}, {} -> {}{}{}) ", px, py, r, g, b);
         *pixel = Rgb([r, g, b])
     }
 
